56130a2798eb096516b78ee2c53ae163e8e9c904
Loowi <treialt@gmail.com>
2020-06-26 13:34:38 +0200

    First commit

--- code/player.py
+++ code/player.py
@@ -25,40 +25,59 @@ class Player:
         self.session = session
         self.moves = []
 
-
     def createNodes(self, fen):
         root = MctsTree(self.session, fen)
         root.addChildren(root)
         return root
-        # root.addChildren(root)
 
     def mtscMove(self, fen, player, enumDict):
-         
-        # create root node with children
-        # pick the move according to np.random.choice
-        # go down, pick move:
-            # if no children, create children, update stats and go up
-            # if children, pick one and continue until stop criteria is hit
-            # if stop criteria is hit, go up and update stats on the way
-            # continue as many times as resources permit
-        root = self.createNodes(fen)
-
         for i in range(10):
-            print(1, root)
-            probs = [child.adjust_value for child in root.children]
-            probs = np.array(probs)
-            probs /= probs.sum()
+            if i == 0:
+                node = self.createNodes(fen)
+                print(1, node)
 
-            pickChild = np.random.choice(root.children, p=probs)
-            print(2, pickChild)
-            
-        return root.adjust_value
+            finished, result = self.game_over(node.fen)
 
+            if finished:  # recursion to the top, update values based on result
+                self.update_values(result)  # todo: recursion
 
+            elif not node.children:  # Spawn a child and recursion to the top
+                self.createNodes(fen)
+                self.update_values(result)
 
+            else:  # pick a child and go deeper
+                node = next_step(x):
 
+        move = self.returnBestMove()
 
+        return move
 
+    def game_over(self, fen): # place all your conditions here
+        board = chess.Board(fen)
+        finished = board.is_game_over()
+        result = board.result()
+        return finished, result
+
+    def next_step(self, x):
+        probs = [child.adjust_value for child in node.children]
+        probs = np.array(probs)
+        probs /= probs.sum()
+        node = np.random.choice(node.children, p=probs)
+        return node
+
+    def returnBestMove(self, children):
+        best_value = -inf
+        for child in children:
+            if child.adjust_value > best_value:  # should be probability based
+                best_move = child.move
+
+    def update_values(self, node):  # recursion
+        if node.parent:
+            parent = node.parent
+            parent.value = parent.mean_value + node.mean_value
+            return(update_values(mean_value))
+        else:
+            return 1
 
 
     def moveSimple(self, fen, enumDict):  # Create legitimate moves and pick one
@@ -71,16 +90,17 @@ class Player:
 
             return move, value
 
-        else:        
+        else:
             fen = af.fenToTensor(fen)
             inputState = np.expand_dims(fen, axis=0)
             inputState = inputState.astype(np.float32)
-        
+
             input_name = self.session.get_inputs()[0].name
             output_name_1 = self.session.get_outputs()[0].name
             output_name_2 = self.session.get_outputs()[1].name
-            
-            policy, value = self.session.run([output_name_1, output_name_2], {input_name: inputState})
+
+            policy, value = self.session.run(
+                [output_name_1, output_name_2], {input_name: inputState})
 
             # policy, value = self.model.predict(inputState)
 
@@ -93,7 +113,6 @@ class Player:
             return move, value
 
 
-
 # def moveSimple(self, fen, enumDict):  # Create legitimate moves and pick one
 #         board = chess.Board(fen)
 #         if self.model is None:
@@ -104,7 +123,7 @@ class Player:
 
 #             return move, value
 
-#         else:        
+#         else:
 #             fen = af.fenToTensor(fen)
 #             inputState = np.expand_dims(fen, axis=0)
 #             policy, value = self.model.predict(inputState)
@@ -116,4 +135,3 @@ class Player:
 #             self.moves.append(move)
 
 #             return move, value
-
